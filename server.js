ca4b074a8064e1560faff5e8ad575558b344789b
kishorekumaru <kishorekumaru@gmail.com>
2014-07-14 23:53:45 +1000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,51 @@
+// JavaScript Document
+
+var mongo = require("mongodb").MongoClient;
+var client = require('socket.io').listen(8080).sockets;
+
+mongo.connect('mongodb://127.0.0.1/chat', function(err, db){
+	if(err) throw err;
+
+	
+	client.on('connection', function(socket){
+		
+		var col = db.collection('messages'),
+		 	sendStatus = function(statusMsg){
+				socket.emit('status', statusMsg);
+			};
+	
+	
+		//Display all the messages from Monogo DB
+		col.find().limit(100).sort({_id: 1}).toArray(function(err, res){
+		
+			if(err) throw err;
+			socket.emit('output', res);
+		});
+		
+		//Wai for input
+		socket.on('input', function(data){
+			var name = data.name;
+			var message = data.message;
+			//Regular Expression to identify the whitespace
+		
+			var whitespacePattern = /^\s*$/;
+			
+			if(whitespacePattern.test(name) || 
+					whitespacePattern.test(message)){
+					console.log("not inserted");
+					sendStatus('Name and message is required');
+			}else{
+			col.insert({name:name, message:message}, function(){
+				//Emit latest messages to all clients
+				client.emit('output', [data]);
+				
+				sendStatus({
+					message: "Message sent",
+					clear:true
+				});
+			});	
+			}
+		});
+	});
+
+});
